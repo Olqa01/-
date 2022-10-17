5777bc5cba1942d321a5bc794a1af372d5eb01b4
unknown <you@example.com>
2022-10-17 22:28:55 +0300

    Homework

---
+++ Task 4/Program.cs
@@ -0,0 +1,19 @@
+﻿// Task 4. Выяснить является ли число чётным
+
+int a;                             
+            Console.Write("введите число: ");
+            a= Console.Read();
+ 
+            if (a % 2==1)
+            {
+                Console.WriteLine("число нечетное");
+           
+            }
+            else
+            {
+                Console.WriteLine("Число четное");
+            }
+ 
+ 
+ 
+            Console.ReadKey();
