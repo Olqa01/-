e996e05d768edf890f7cd4e47abe42c5c834d070
unknown <you@example.com>
2022-10-17 22:28:42 +0300

    Homework

---
+++ Task 3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Task 3. Найти максимальное из трех чисел
+
+int numberA = new Random().Next(1,55);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(55,110);
+Console.WriteLine(numberB);
+int numberC = new Random().Next(110,165);
+Console.WriteLine(numberC);
+
+int max = numberA;
+
+if (numberA > max) max = numberA;
+if (numberB  >max) max = numberB;
+if (numberC > max) max = numberC;
+
+Console.Write("max = ");
+Console.WriteLine(max);
